5d6ec50ec53506fe580f266e0692d514020dce1c
Sean Anteau <santeau@gmail.com>
2018-10-18 16:38:52 -0500

    Adding Address routes

---
+++ models/address.js
@@ -0,0 +1,14 @@
+export default (sequelize, DataTypes) => {
+    const Address = sequelize.define('Address',{
+        address1: DataTypes.STRING,
+        address2: DataTypes.STRING,
+        city: DataTypes.STRING,
+        state: DataTypes.STRING,
+        zipcode: DataTypes.INTEGER,
+        business: DataTypes.INTEGER,
+        lat: DataTypes.FLOAT,
+        lng: DataTypes.FLOAT,
+        placeId: DataTypes.STRING,
+        country: DataTypes.STRING
+    });
+}
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,27 @@
 import Router from "koa-router";
 import status from "./status";
 import business from "./business";
+import address from "./address";
+import trace from "./trace";
+import pin from "./pin";
+import user from "./user";
 const router = new Router();
 
 router.use("/", status);
 
 // Businesses
 router.use("/businesses", business);
+
+// Addresses
+router.use("/addresses", address);
+
+// Trace
+router.use("/traces", trace);
+
+// Pin
+router.use("/pins", pin);
+
+// User
+router.use("/users", user);
+
 export default router;
