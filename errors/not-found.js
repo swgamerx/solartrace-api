10fc0c46d99cd269af0e77f63809ce6e39af49fc
Sean <santeau@gmail.com>
2020-04-23 14:58:56 -0500

    Removing all files to switch to python Flask

--- errors/not-found.js
+++
@@ -1,7 +0,0 @@
-export default class NotFoundError extends Error {
-  constructor(modelName, id) {
-    super();
-    this.modelName = modelName;
-    this.id = id;
-  }
-}
--- models/business.js
+++
@@ -1,9 +0,0 @@
-export default (sequelize, DataTypes) => {
-  const Business = sequelize.define("Business", {
-    name: DataTypes.STRING
-  });
-  Business.associate = function(models) {
-    Business.hasMany(models.Address); // all the addresses for the business
-  };
-  return Business;
-};
